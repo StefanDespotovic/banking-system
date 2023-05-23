69b97352afc8758a45ff0893f443d2ddfbada843
StefanDespotovic <stefadesa@gmail.com>
2023-05-23 11:32:57 +0200

    edited transactions.js

--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -105,8 +105,7 @@ const Transactions = ({ transactions }) => {
               </div>
               <div>
                 <Price>
-                  {transaction.sign}
-                  {transaction.value}
+                  {transaction.sign} {transaction.value}
                 </Price>
                 <Currency>USD</Currency>
               </div>
