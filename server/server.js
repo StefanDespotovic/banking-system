69c5600804face07060a57db6ae0b0b56fa1d284
StefanDespotovic <stefadesa@gmail.com>
2023-06-27 13:05:01 +0200

    test

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const port = process.env.DATA_PORT || 3306;
 
 const connection = require("./db");
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 const userRoutes = require("./userRoutes");
 app.use("/api", userRoutes);
